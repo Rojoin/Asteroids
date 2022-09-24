b39a776623021f1c752cdde8dfae97bfe4e52ca5
Rojoin <nachoarrastua@gmail.com>
2022-09-24 14:38:19 -0300

    Created Collisions

---
+++ src/system/collisionLogic.cpp
@@ -0,0 +1,34 @@
+#include "collisionLogic.h"
+#include  "raymath.h"
+
+bool isCircleCircleColliding(Circle circle1, Circle circle2)
+{
+	
+	float distanceY = circle1.position.y -circle2.position.y;
+	float distanceX = circle1.position.x -circle2.position.x;
+	float distance = sqrt(abs(distanceX) + abs(distanceY));
+
+	if (distance <= circle1.radius + circle2.radius)
+	{
+		return true;
+	}
+
+		return false;
+}
+
+bool isPointRecColliding(Vector2 mouse, Rectangle rec) //Check if works
+{
+	int rightEdge = rec.x + rec.width / 2;
+	int leftEdge = rec.x;
+	int upEdge = rec.y;
+	int downEdge = rec.y + rec.height / 2;
+
+	if (mouse.x >= leftEdge && mouse.x <= rightEdge && mouse.y >= upEdge && mouse.y <= downEdge)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
