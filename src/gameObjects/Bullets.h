a2775bebf6f1c32f02b0703a6c7b60a4decadefd
Rojoin <nachoarrastua@gmail.com>
2022-09-24 14:39:54 -0300

    Creating the bullet basics + some methods

--- src/gameObjects/Bullets.h
+++ src/gameObjects/Bullets.h
@@ -1,15 +1,24 @@
 #pragma once
 #include <raylib.h>
-
+#include "../system/circle.h"
 #include "GameObjectsEnums.h"
+#include "spaceShip.h"
 
-struct Bullets
+struct Bullet
 {
 	Asteroids::BulletType type;
-	Vector2 position;
-	float size;
+	Texture2D texture;
+	Circle circle;
+	Vector2 aceleration;
 	bool isActive;
-	
 
 };
+
+void moveBullet(Bullet& bullet);
+
+void deactivateBullet(Bullet& bullet);
+void activateBullet(Bullet& bullet);
+void resetBulletPosition(Bullet& bullet, SpaceShip ship);
+
+
 
