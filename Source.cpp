14f234c92bb0807e6357094beb9a39f7f792d662
Rojoin <nachoarrastua@gmail.com>
2022-09-26 13:24:30 -0300

    Testing new Source

--- Source.cpp
+++ Source.cpp
@@ -1,124 +1,38 @@
 
 #include "raylib.h"
-#include "src/gameLogic/collisionLogic.h"
-#include "src/gameObjects/spaceShip.h"
-#include "src/gameLogic/movementLogic.h"
-#include "src/gameObjects/Bullets.h"
-#include "src/gameObjects/Asteroid.h"
-#include "src/system/draw.h"
+#include "src/gameLogic/gameLogic.h"
 
-extern SpaceShip spaceShip;
-GameObjects::Asteroid bigAsteroid[10];
-GameObjects::Asteroid mediumAsteroid[20];
-GameObjects::Asteroid smallAsteroid[40];
-int bigAsteroidCount = 10;
-int mediumAsteroidCount = 0;
-int smallAsteroidCount = 0;
+#include "src/system/draw.h"
+#include "src/gameObjects/spaceShip.h"
+
+
 int main()
 {
-	InitWindow(1024, 768, "Ship Example");
+	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+	InitWindow(1920, 1000, "Ship Example");
+	SetWindowMinSize(1024, 768);
 	Font customFont = LoadFontEx("resources/LoftygoalsRegular-9Y5Xy.otf", 96, 0, 0);
-	Texture2D shipTexture = LoadTexture("resources/pizzaTiledMap.png");
-	Texture2D bulletTexture = LoadTexture("resources/olive.png");
-	Texture2D asteroidTexture = LoadTexture("resources/bigHand.png");
+	extern Texture2D shipTexture;
+	extern Texture2D bulletTexture;
+	extern Texture2D asteroidTexture;
+	extern Texture2D asteroidMediumTexture;
+ shipTexture = LoadTexture("resources/pizzaTiledMap.png");
+ bulletTexture = LoadTexture("resources/olive.png");
+ asteroidTexture = LoadTexture("resources/bigHand.png");
+ asteroidMediumTexture = LoadTexture("resources/mediumHand.png");
 
-	for (int i = 0; i < 10; ++i)
-	{
-		bigAsteroid[i] = GameObjects::createAsteroid();
-		bigAsteroid[i].texture = asteroidTexture;
-	}
-	for (int i = 0; i < 20; ++i)
-	{
-		mediumAsteroid[i] = GameObjects::createMediumAsteroid();
-		mediumAsteroid[i].texture = asteroidTexture;
-	}
-	for (int i = 0; i < 20; ++i)
-	{
-		smallAsteroid[i] = GameObjects::createSmallAsteroid();
-		smallAsteroid[i].texture = asteroidTexture;
-	}
+	extern SpaceShip spaceShip;
+	
+	initGame();
+
 	GenTextureMipmaps(&customFont.texture);
 	SetTextureFilter(customFont.texture, TEXTURE_FILTER_TRILINEAR);
-	Vector2 spacePosition = { (float)GetScreenWidth() / 2,(float)GetScreenHeight() / 2 };
-	spaceShip = initSpaceShip(shipTexture, spacePosition, 0, 1);
-	initBullets(bulletTexture);
+
 	while (!WindowShouldClose())
 	{
-		GameLogic::moveSpaceShip(spaceShip);
-		for (int i = 0; i < 10; i++)
-		{
-			GameObjects::moveAsteroid(bigAsteroid[i]);
-			GameObjects::moveBullet(spaceShip.bullet[i]);
-		}
-		for (int i = 0; i < 20; ++i)
-		{
-			GameObjects::moveAsteroid(mediumAsteroid[i]);
-		}for (int i = 0; i < 40; ++i)
-		{
-			GameObjects::moveAsteroid(smallAsteroid[i]);
-		}
-
-		updateShip();
-		updateBullet();
-		if (IsKeyPressed(KEY_SPACE))
-		{
-			activateBullet();
-		} if (IsKeyDown(KEY_V))
-		{
-			//resetBulletPosition();
-		}
-		if (Inputs::isMouseKeyDown(MOUSE_BUTTON_RIGHT))
-		{
-			for (int i = 0; i < 10; ++i)
-			{
-
-				GameObjects::activateAsteroid(bigAsteroid[i]);
-			
-			}
-		}
-		changeShipPosition();
-		for (int i = 0; i < 10; i++)
-		{
-			GameLogic::moveAsteroidAcrossScreen(bigAsteroid[i]);
-			GameLogic::asteroidSpaceShipCollision(bigAsteroid[i], spaceShip);
-			for (int j = 0; j < 10; j++)
-			{
-
-				GameLogic::asteroidBulletCollision(bigAsteroid[j], spaceShip.bullet[i]);
-			}
-			for (int j = 0; j < 20; j++)
-			{
-			GameLogic::moveAsteroidAcrossScreen(mediumAsteroid[j]);
-				GameLogic::asteroidSpaceShipCollision(mediumAsteroid[i], spaceShip);
-				GameLogic::asteroidBulletCollision(mediumAsteroid[j], spaceShip.bullet[i]);
-			}for (int j = 0; j < 40; j++)
-			{
-			GameLogic::moveAsteroidAcrossScreen(smallAsteroid[j]);
-				GameLogic::asteroidSpaceShipCollision(smallAsteroid[j], spaceShip);
-				GameLogic::asteroidBulletCollision(smallAsteroid[j], spaceShip.bullet[i]);
-			}
-			//if (!bigAsteroid[i].isActive)
-			//{
-			//	resetAsteroid(bigAsteroid[i]);
-			//}
-		}
+		playGame();
 		BeginDrawing();
-		for (int i = 0; i < 10; i++)
-		{
-			GameObjects::drawAsteroid(bigAsteroid[i]);
-
-			GameObjects::drawBullet(spaceShip.bullet[i]);
-		}
-		for (int i = 0; i < 20; ++i)
-		{
-			GameObjects::drawAsteroid(mediumAsteroid[i]);
-		}
-		for (int i = 0; i < 40; ++i)
-		{
-			
-			GameObjects::drawAsteroid(smallAsteroid[i]);
-		}
-		drawShip();
+		drawGame();
 		drawText(TextFormat("SCORE: %1i", spaceShip.score), 0, 100, 50, WHITE, customFont);
 		ClearBackground(BLACK);
 		EndDrawing();
