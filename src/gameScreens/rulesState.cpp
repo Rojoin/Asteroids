962cc5ec16af7b6693af6fe8a7ac52316cf48e1a
Rojoin <nachoarrastua@gmail.com>
2022-10-08 21:06:04 -0300

    Updated rules state

--- src/gameScreens/rulesState.cpp
+++ src/gameScreens/rulesState.cpp
@@ -9,17 +9,15 @@ Button escapeButton = createButton(" GO BACK", RED);
 void statesRules()
 {
 	Vector2 mousePoint = GetMousePosition();
-	escapeButton = createButton(0, (float)GetScreenHeight() - escapeButton.rec.height*2, escapeButton.buttonTittle, escapeButton.color);
+	escapeButton = createButton(0, (float)GetScreenHeight() - escapeButton.rec.height * 2, escapeButton.buttonTittle, escapeButton.color);
 	if (CheckCollisionPointRec(mousePoint, escapeButton.rec))
 	{
 		escapeButton.isOverThisButton = true;
-
 
 		if (IsMouseButtonReleased(MOUSE_LEFT_BUTTON))
 		{
 			setGameState(GameStates::Menu);
 		}
-
 	}
 	else
 	{
@@ -30,32 +28,35 @@ void drawRules()
 {
 
 	float width = static_cast<float>(GetScreenWidth());
-	float height= static_cast<float>(GetScreenHeight());
+	float height = static_cast<float>(GetScreenHeight());
 	ClearBackground(BLACK);
 	drawButton(escapeButton);
-	std::string rulesTitle = "Rules";
-	float fontSize = 3 * width/ 190;
-	std::string firstSentence = "The Last Slice is a survival game";
-	std::string secondSentence = " where you try to not get eaten";
+	float fontSize = 3 * width / 190;
+	const std::string rulesTitle = "Rules";
+	const std::string rulesFirstSentence = "The Last Slice is a survival game";
+	const std::string rulesSecondSentence = " where you try to not get eaten.";
+	const std::string rulesThirdSentence = "WatchOut! The hands will try to catch you!";
+	const std::string rulesForthSentence = "You have 3 lives";
+	const std::string rulesFifthSentence = "You will lose one if they caught you";
+	const std::string rulesSixthSentence = "if you lose all your lives its GameOver!";
+	const std::string controlsTitle = "Controls";
+	const std::string controlsFirstSentence = "Use the LeftClick to shoot";
+	const std::string controlsSecondSentence = "Use the RightClick to move towards your mouse";
 
-	std::string thirdSentence = "Use the LeftClick to shoot";
-	std::string forthSentence = "Use the RightClick to move towards your mouse";
-	std::string athirdSentence = "WatchOut! The hands will try to catch you!";
-	std::string aforthSentence = "You have 3 lives and you lose one every time an enemy touch you"; 
-	std::string fifthSentence = "if you lose all your lives its GameOver!";
-	drawText(rulesTitle,	  width/ 2.0f     - static_cast<float>(rulesTitle.length())*1.5f *fontSize, height / 10,fontSize* 8.0f, DARKBROWN, customFont);
-	drawText(firstSentence, width/ 2.0f  - static_cast<float>(firstSentence.length())/1.5f  * fontSize, height / 4, fontSize*2.0f, BLACK, customFont);
-	drawText(secondSentence,width / 2.0f - static_cast<float>(secondSentence.length())/1.5f * fontSize, height / 3.5f, fontSize * 2.0f, BLACK, customFont);
-	drawText(thirdSentence, width / 2.0f  - static_cast<float>(thirdSentence.length())/1.5f  *fontSize, height / 3.0f, fontSize * 2.0f, BLACK, customFont);
-	drawText(forthSentence, width / 2.0f  - static_cast<float>(forthSentence.length())/1.5f  *fontSize, height / 2.5f, fontSize * 2.0f, BLACK, customFont);
+	drawText(rulesTitle,         width / 2.0f - static_cast<float>(rulesTitle.length()) * 1.5f * fontSize, height / 40, fontSize * 8.0f, DARKBROWN, customFont);
+	drawText(rulesFirstSentence, width / 2.0f - static_cast<float>(rulesFirstSentence.length()) / 1.5f * fontSize, height / 5, fontSize * 2.0f, BLACK, customFont);
+	drawText(rulesSecondSentence,width / 2.0f - static_cast<float>(rulesSecondSentence.length()) / 1.5f * fontSize, height / 4.0f, fontSize * 2.0f, BLACK, customFont);
+	drawText(rulesThirdSentence, width / 2.0f - static_cast<float>(rulesThirdSentence.length()) / 1.5f * fontSize, height / 3.5f, fontSize * 2.0f, BLACK, customFont);
+	drawText(rulesForthSentence, width / 2.0f - static_cast<float>(rulesForthSentence.length()) / 1.5f * fontSize, height / 3.0f, fontSize * 2.0f, BLACK, customFont);
+	drawText(rulesFifthSentence, width / 2.0f - static_cast<float>(rulesFifthSentence.length()) / 1.5f * fontSize, height / 2.5f, fontSize * 2.0f, BLACK, customFont);
+	drawText(rulesSixthSentence, width / 2.0f - static_cast<float>(rulesSixthSentence.length()) / 1.5f * fontSize, height / 2.2f, fontSize * 2.0f, BLACK, customFont);
+
+
+	drawText(controlsTitle, width / 2.0f - static_cast<float>(controlsTitle.length()) * 1.5f * fontSize, height / 2, fontSize * 8.0f, DARKBROWN, customFont);
+	drawText(controlsFirstSentence, width / 2.0f - static_cast<float>(controlsFirstSentence.length()) / 1.5f * fontSize, height / 1.5f, fontSize * 2.0f, BLACK, customFont);
+	drawText(controlsSecondSentence, width / 2.0f - static_cast<float>(controlsSecondSentence.length()) / 1.5f * fontSize, height / 1.4f, fontSize * 2.0f, BLACK, customFont);
 	DrawRectangle(GetScreenWidth() / 2, 0, 10, GetScreenHeight(), BLUE);
-	//DrawText(firstSentence.c_str(), GetScreenWidth() / 3 - firstSentence.length(), (GetScreenHeight() / 4 + fontSize), fontSize, BLACK);
-	//DrawText(secondSentence.c_str(), GetScreenWidth() / 3 - secondSentence.length(), (GetScreenHeight() / 5 + fontSize), fontSize, BLACK);
-	//DrawText(thirdSentence.c_str(), GetScreenWidth() / 3 - thirdSentence.length(), (GetScreenHeight() / 2 - GetScreenHeight() / 5) + fontSize, fontSize, BLACK);
-	//DrawText(forthSentence.c_str(), GetScreenWidth() / 3 - forthSentence.length(), (GetScreenHeight() / 2 - GetScreenHeight() / 6) + fontSize, fontSize, BLACK);
-	//DrawText(fifthSentence.c_str(), GetScreenWidth() / 3 - fifthSentence.length(), (GetScreenHeight() / 2 - GetScreenHeight() / 7) + fontSize, fontSize, BLACK);
-	//DrawText(rulesTitle.c_str(), ((GetScreenWidth() / 2) - rulesTitle.length() * fontSize), GetScreenHeight() / 10, fontSize * 4, RED);
-
+	
 	drawButton(escapeButton);
 
 
