40abf385786290d225b851b06c8fd9fde9dbcda2
Rojoin <nachoarrastua@gmail.com>
2022-10-11 12:38:53 -0300

    Clean comments

--- src/gameScreens/button.cpp
+++ src/gameScreens/button.cpp
@@ -10,15 +10,12 @@ void drawButton(Button button)
 	float roundness = 10;
 	int segments = 8;
 	DrawRectangleRounded(button.rec, roundness, segments, button.color);
-	//DrawRectangleRec(button.rec, button.color);
 	
 	drawText(button.buttonTittle, button.rec.x, button.rec.y + button.rec.height / 3, 25, BLACK, customFont);
-	//DrawText(button.buttonTittle.c_str(), button.rec.x, button.rec.y + button.rec.height / 3, 3 * GetScreenWidth() / 190, BLACK);
 
 	if (button.isOverThisButton)
 	{
 		DrawRectangleRoundedLines(button.rec, roundness, segments, 5, BLACK);
-	//	DrawRectangleLinesEx(button.rec, 5, RED);
 	}
 
 }
@@ -30,45 +27,39 @@ void drawButtonTranslucent(Button button)
 	translucentFont.a /= 2;
 	float roundness = 10;
 	int segments = 8;
-	DrawRectangleRounded(button.rec, roundness, segments, translucentButton);
-	
-	//DrawRectangleRec(button.rec, button.color);
 
+	DrawRectangleRounded(button.rec, roundness, segments, translucentButton);
 	drawText(button.buttonTittle, button.rec.x, button.rec.y + button.rec.height / 3, 25, translucentFont, customFont);
-	//DrawText(button.buttonTittle.c_str(), button.rec.x, button.rec.y + button.rec.height / 3, 3 * GetScreenWidth() / 190, BLACK);
 
 	if (button.isOverThisButton)
 	{
 		DrawRectangleRounded(button.rec, roundness, segments, button.color);
 		drawText(button.buttonTittle, button.rec.x, button.rec.y + button.rec.height / 3, 25, BLACK, customFont);
 		DrawRectangleRoundedLines(button.rec, roundness, segments, 5, BLACK);
-		//	DrawRectangleLinesEx(button.rec, 5, RED);
 	}
 }
 Button createButton(std::string buttonTitle, Color color)
 {
 	Button button;
-	//float width = GetScreenWidth() / 9;
-	//float height = GetScreenHeight() / 20;
 
-	button.rec = { (float)GetScreenWidth() / 2 - buttonWidth / 2, (float)GetScreenHeight() / 3, buttonWidth, buttonHeight };
+	button.rec = { static_cast<float>(GetScreenWidth() / 2 - buttonWidth / 2),  static_cast<float>(GetScreenHeight() / 3), buttonWidth, buttonHeight };
 	button.buttonTittle = buttonTitle;
 	button.isSelected = false;
 	button.isOverThisButton = false;
 	button.color = color;
+
 	return button;
 }
 Button createButton(float x, float y, std::string buttonTitle, Color color)
 {
 	Button button;
-	//float width = GetScreenWidth() / 9;
-	//float height = GetScreenHeight() / 20;
 
 	button.rec = { x,y, buttonWidth, buttonHeight };
 	button.buttonTittle = buttonTitle;
 	button.isSelected = false;
 	button.isOverThisButton = false;
 	button.color = color;
+
 	return button;
 }
 Button createButton(float x, float y, float width, float height, std::string buttonTitle, Color color)
@@ -80,5 +71,6 @@ Button createButton(float x, float y, float width, float height, std::string but
 	button.isSelected = false;
 	button.isOverThisButton = false;
 	button.color = color;
+
 	return button;
 }
