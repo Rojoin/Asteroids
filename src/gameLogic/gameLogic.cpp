14d2cfee33eaa0a372f2574cbe12cadce3eb058b
Rojoin <nachoarrastua@gmail.com>
2022-10-11 00:23:55 -0300

    Added Special Asteroid in Game

--- src/gameLogic/gameLogic.cpp
+++ src/gameLogic/gameLogic.cpp
@@ -15,10 +15,11 @@
 extern SpaceShip spaceShip;
 extern Font customFont;
 extern GameStates gameStates;
-const int maxBullets = 10;
-const int maxBigAsteroids = 10;
+const int maxBullets = 20;
+const int maxBigAsteroids = 4;
 const int maxMediumAsteroids = 40;
 const int maxSmallAsteroids = 80;
+GameObjects::Asteroid specialAsteroid;
 GameObjects::Asteroid bigAsteroid[maxBigAsteroids];
 GameObjects::Asteroid mediumAsteroid[maxMediumAsteroids];
 GameObjects::Asteroid smallAsteroid[maxSmallAsteroids];
@@ -26,21 +27,22 @@ GameObjects::Asteroid smallAsteroid[maxSmallAsteroids];
 int mediumAsteroidCount = 0;
 int smallAsteroidCount = 0;
 
-int bigAsteroidsOnScreen = 10 / 2;
+
 int mediumAsteroidsOnScreen = 40 / 2;
 int smallAsteroidsOnScreen = 80 / 2;
 Texture2D livesTexture;
 Texture2D shipTexture;
 Texture2D bulletTexture;
-Texture2D asteroidTexture;
+Texture2D asteroidSpecialTexture;
+Texture2D asteroidBigTexture;
 Texture2D asteroidMediumTexture;
 Texture2D asteroidSmallTexture;
 Sound deathSound;
 Sound bulletSound;
-Button pauseMenuButton =    createButton(0, 0, buttonWidth, buttonHeight, "PAUSE", GREEN);
+Button pauseMenuButton = createButton(0, 0, buttonWidth, buttonHeight, "PAUSE", GREEN);
 Button continueMenuButton = createButton(0, 0, buttonWidth, buttonHeight, "CONTINUE", BROWN);
-Button restartMenuButton =  createButton(0, 0, buttonWidth, buttonHeight, "RESTART", GREEN);
-Button exitMenuButton =     createButton(0, 0, " EXIT", GREEN);
+Button restartMenuButton = createButton(0, 0, buttonWidth, buttonHeight, "RESTART", GREEN);
+Button exitMenuButton = createButton(0, 0, " EXIT", GREEN);
 bool isGamePaused = false;
 bool isGameOver = false;
 void initGame()
@@ -48,16 +50,19 @@ void initGame()
 	float width = static_cast<float>(GetScreenWidth());
 	float height = static_cast<float>(GetScreenHeight());
 	isGamePaused = false;
-	continueMenuButton = createButton(width / 2 - buttonWidth/2, height / 2- height /8, buttonWidth, buttonHeight, " CONTINUE", DARKGREEN);
-	restartMenuButton = createButton(width / 3 + width / 4 - buttonWidth/2, height / 2.0f - buttonHeight, buttonWidth, buttonHeight, " RESTART", DARKPURPLE);
-	exitMenuButton =    createButton(width / 3 + width / 8 - buttonWidth, height / 2.0f - buttonHeight, buttonWidth, buttonHeight, "   EXIT", RED);
-	pauseMenuButton =  createButton(width / 2 - buttonWidth / 2, 0 + buttonHeight / 2, buttonWidth, buttonHeight, "  PAUSE", DARKGREEN);
+	continueMenuButton = createButton(width / 2 - buttonWidth / 2, height / 2 - height / 8, buttonWidth, buttonHeight, " CONTINUE", DARKGREEN);
+	restartMenuButton = createButton(width / 3 + width / 4 - buttonWidth / 2, height / 2.0f - buttonHeight, buttonWidth, buttonHeight, " RESTART", DARKPURPLE);
+	exitMenuButton = createButton(width / 3 + width / 8 - buttonWidth, height / 2.0f - buttonHeight, buttonWidth, buttonHeight, "   EXIT", RED);
+	pauseMenuButton = createButton(width / 2 - buttonWidth / 2, 0 + buttonHeight / 2, buttonWidth, buttonHeight, "  PAUSE", DARKGREEN);
+	specialAsteroid = GameObjects::createSpecialAsteroid();
+	specialAsteroid.texture = asteroidSpecialTexture;
+	specialAsteroid.isActive = true;
 	for (int i = 0; i < maxSmallAsteroids; ++i)
 	{
-		if (i < 10)
+		if (i < maxBigAsteroids)
 		{
 			bigAsteroid[i] = GameObjects::createBigAsteroid();
-			bigAsteroid[i].texture = asteroidTexture;
+			bigAsteroid[i].texture = asteroidBigTexture;
 		}
 		if (i < maxMediumAsteroids)
 		{
@@ -76,12 +81,16 @@ void initGame()
 void resetGame()
 {
 	isGamePaused = false;
+	specialAsteroid = GameObjects::createSpecialAsteroid();
+	specialAsteroid.texture = asteroidSpecialTexture;
+	specialAsteroid.isActive = true;
+
 	for (int i = 0; i < maxSmallAsteroids; ++i)
 	{
-		if (i < 10)
+		if (i < maxBigAsteroids)
 		{
 			bigAsteroid[i] = GameObjects::createBigAsteroid();
-			bigAsteroid[i].texture = asteroidTexture;
+			bigAsteroid[i].texture = asteroidBigTexture;
 		}
 		if (i < maxMediumAsteroids)
 		{
@@ -90,7 +99,7 @@ void resetGame()
 		}
 
 		smallAsteroid[i] = GameObjects::createSmallAsteroid();
-		smallAsteroid[i].texture = asteroidTexture;
+		smallAsteroid[i].texture = asteroidBigTexture;
 	}
 
 	Vector2 spacePosition = { (float)GetScreenWidth() / 2,(float)GetScreenHeight() / 2 };
@@ -137,6 +146,7 @@ void playGame()
 		}
 
 		GameLogic::moveSpaceShip(spaceShip);
+		GameObjects::changeAsteroidPosition(specialAsteroid);
 		for (int i = 0; i < maxBullets; i++)
 		{
 			GameObjects::changeBulletPosition(spaceShip.bullet[i]);
@@ -148,7 +158,8 @@ void playGame()
 		for (int i = 0; i < maxMediumAsteroids; ++i)
 		{
 			GameObjects::changeAsteroidPosition(mediumAsteroid[i]);
-		}for (int i = 0; i < maxSmallAsteroids; ++i)
+		}
+		for (int i = 0; i < maxSmallAsteroids; ++i)
 		{
 			GameObjects::changeAsteroidPosition(smallAsteroid[i]);
 		}
@@ -161,6 +172,14 @@ void playGame()
 		changeShipPosition();
 		for (int i = 0; i < maxBullets; i++)
 		{
+			GameLogic::moveAsteroidAcrossScreen(specialAsteroid);
+			if (GameLogic::asteroidSpaceShipCollision(specialAsteroid, spaceShip))
+			{
+				resetGame();
+			}
+
+			GameLogic::asteroidBulletCollision(specialAsteroid, spaceShip.bullet[i]);
+
 			for (int j = 0; j < maxBigAsteroids; j++)
 			{
 				GameLogic::moveAsteroidAcrossScreen(bigAsteroid[i]);
@@ -181,7 +200,10 @@ void playGame()
 					resetGame();
 				}
 				GameLogic::asteroidBulletCollision(mediumAsteroid[j], spaceShip.bullet[i]);
-			}for (int j = 0; j < maxSmallAsteroids; j++)
+			
+
+			}
+			for (int j = 0; j < maxSmallAsteroids; j++)
 			{
 				GameLogic::moveAsteroidAcrossScreen(smallAsteroid[j]);
 				if (GameLogic::asteroidSpaceShipCollision(smallAsteroid[j], spaceShip))
@@ -190,11 +212,19 @@ void playGame()
 				}
 				GameLogic::asteroidBulletCollision(smallAsteroid[j], spaceShip.bullet[i]);
 			}
-			if (!bigAsteroid[i].isActive)
+
+		}
+		for (int j = 0; j < maxBigAsteroids; ++j)
+		{
+
+			if (!bigAsteroid[j].isActive)
 			{
-				resetAsteroid(bigAsteroid[i]);
+				resetAsteroid(bigAsteroid[j]);
 			}
 		}
+		
+
+		updateSpecialAsteroid(specialAsteroid, spaceShip.position);
 	}
 	else if (isGamePaused)
 	{
@@ -265,18 +295,18 @@ void drawPauseMenu()
 }
 void drawUI()
 {
-	
+
 	std::string playerScore = TextFormat("Score:%0.0F", spaceShip.score);
 	std::string maxScore = TextFormat("Max:%1i", spaceShip.score);
 	Vector2 scoreMeasure = MeasureTextEx(customFont, playerScore.c_str(), 50, 0);
 	Vector2 maxScoreMeasure = MeasureTextEx(customFont, maxScore.c_str(), 50, 0);
-	drawText(playerScore , 0, 0, 50, BLACK, customFont);
+	drawText(playerScore, 0, 0, 50, BLACK, customFont);
 	for (int i = 0; i < spaceShip.lives; i++)
 	{
 		drawTexture(livesTexture, { livesTexture.width * (i + 1.0f),scoreMeasure.y }, 0, 1, WHITE);
 	}
 	//Implementar MaxScore
-	drawText(maxScore, GetScreenWidth()-maxScoreMeasure.x*1.5f, 0, 50, BLACK, customFont);
+	drawText(maxScore, GetScreenWidth() - maxScoreMeasure.x * 1.5f, 0, 50, BLACK, customFont);
 }
 void drawGame()
 {
@@ -299,7 +329,7 @@ void drawGame()
 
 		GameObjects::drawAsteroid(smallAsteroid[i]);
 	}
-	
+	GameObjects::drawAsteroid(specialAsteroid);
 	drawShip();
 	drawButtonTranslucent(pauseMenuButton);
 	drawUI();
